7291b93cb06477cf4fbab8209655f3bdeed2247f
datcosmi <ivan03idk@gmail.com>
2024-09-28 03:25:48 -0600

    home screen v2

--- app/index.tsx
+++ app/index.tsx
@@ -3,10 +3,11 @@ import {
   View,
   Text,
   StyleSheet,
-  Image,
   TouchableOpacity,
   ScrollView,
 } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import Icon from "react-native-vector-icons/Ionicons"; // Importamos los íconos
 
 const HomeScreen = () => {
   return (
@@ -17,11 +18,20 @@ const HomeScreen = () => {
           <Text style={styles.menuIcon}>☰</Text>
         </TouchableOpacity>
       </View>
+
       <ScrollView style={styles.container}>
         <View style={styles.mainContent}>
           <View style={styles.txtContainer}>
-            <Text style={styles.title}>¿Quienes somos?</Text>
-
+            <LinearGradient colors={["#ff9a9e", "#ffc79a"]} style={styles.icon}>
+              <Icon
+                name="business"
+                size={50}
+                color="#fff"
+                style={styles.icon}
+              />
+            </LinearGradient>
+            <Text style={styles.title}>¿Quiénes somos?</Text>
+            <View style={styles.lineaColor} />
             <Text style={styles.subtitle}>
               Somos Rancheritos Tech, una empresa dedicada al desarrollo de
               software y a la implementación de Tecnologías de la Información
@@ -30,14 +40,23 @@ const HomeScreen = () => {
           </View>
 
           <View style={styles.txtContainer}>
-            <Text style={styles.title2}>Misión</Text>
+            <LinearGradient colors={["#a1c4fd", "#9edbf7"]} style={styles.icon}>
+              <Icon name="ribbon" size={50} color="#fff" style={styles.icon} />
+            </LinearGradient>
+            <Text style={styles.title}>Misión</Text>
+            <View style={styles.lineaColor2} />
             <Text style={styles.subtitle}>
               Crear soluciones de software innovadoras y accesibles que impulsen
               el crecimiento y la eficiencia de nuestros clientes.
             </Text>
           </View>
+
           <View style={styles.txtContainer}>
-            <Text style={styles.title2}>Visión</Text>
+            <LinearGradient colors={["#fbc2eb", "#c7a6ee"]} style={styles.icon}>
+              <Icon name="rocket" size={50} color="#fff" style={styles.icon} />
+            </LinearGradient>
+            <Text style={styles.title}>Visión</Text>
+            <View style={styles.lineaColor3} />
             <Text style={styles.subtitle}>
               Ser reconocidos como una empresa líder en el desarrollo de
               soluciones tecnológicas innovadoras, impulsando el crecimiento de
@@ -71,16 +90,16 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 10,
     padding: 20,
-    backgroundColor: "#f8f8f8",
-    borderRadius: 15, // Bordes redondeados
-    shadowColor: "#8a8a8a", // Color de la sombra
+    backgroundColor: "#fff",
+    borderRadius: 30,
+    shadowColor: "#c4c4c4",
     shadowOffset: {
       width: 0,
       height: 2,
     },
     shadowOpacity: 0.25,
-    shadowRadius: 3.84, // Difuminar sombra en iOS
-    elevation: 5, // Sombra en Android
+    shadowRadius: 10,
+    elevation: 5,
   },
   header: {
     flexDirection: "row",
@@ -91,7 +110,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   logo: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: "bold",
     letterSpacing: 2,
   },
@@ -99,47 +118,65 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   menuIcon: {
-    fontSize: 20,
+    fontSize: 25,
     fontWeight: "bold",
   },
   mainContent: {
     padding: 20,
   },
+  icon: {
+    marginBottom: 10,
+    margin: "auto",
+    padding: 10,
+    paddingBottom: 7,
+    paddingTop: 7,
+    borderRadius: 70,
+    shadowColor: "#8a8a8a",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 5,
+  },
   title: {
-    fontSize: 36,
+    fontSize: 24,
     fontWeight: "bold",
-    lineHeight: 40,
-    marginVertical: 20,
+    lineHeight: 30,
+    marginVertical: 10,
     color: "#616161",
   },
-  title2: {
-    fontSize: 30,
-    fontWeight: "bold",
-    lineHeight: 40,
-    marginVertical: 20,
-    textAlign: "center",
-    color: "#616161",
+  lineaColor: {
+    height: 3,
+    width: "65%",
+    backgroundColor: "#ff9a9e",
+    marginBottom: 10,
+  },
+  lineaColor2: {
+    height: 3,
+    width: "25%",
+    backgroundColor: "#a1c4fd",
+    marginBottom: 10,
+  },
+  lineaColor3: {
+    height: 3,
+    width: "25%",
+    backgroundColor: "#fbc2eb",
+    marginBottom: 10,
   },
   subtitle: {
     fontSize: 14,
     color: "#999",
-    letterSpacing: 2,
     marginVertical: 10,
-  },
-  description: {
-    fontSize: 16,
-    color: "#666",
-    marginBottom: 30,
   },
   buttonsContainer: {
     flexDirection: "row",
     marginBottom: 20,
     marginTop: 30,
-    justifyContent: "center",
-    alignItems: "center",
   },
   buttonPrimary: {
-    backgroundColor: "#e07d64",
+    backgroundColor: "#f0927b",
     paddingVertical: 15,
     paddingHorizontal: 30,
     borderRadius: 5,
@@ -161,15 +198,6 @@ const styles = StyleSheet.create({
     color: "#999",
     fontWeight: "bold",
     fontSize: 16,
-  },
-  imageContainer: {
-    padding: 20,
-    alignItems: "center",
-  },
-  image: {
-    width: "100%",
-    height: 200,
-    resizeMode: "contain",
   },
 });
 
