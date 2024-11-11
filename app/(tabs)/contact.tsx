06926441843603c0ff8b0e1f301ce74e2a57e79a
Samuel Reyna González <118919573+Meiuwu2001@users.noreply.github.com>
2024-11-11 00:50:52 -0600

    contact form

--- app/(tabs)/contact.tsx
+++ app/(tabs)/contact.tsx
@@ -4,7 +4,6 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import { LinearGradient } from "expo-linear-gradient";
 import React, { useState, useEffect } from "react";
 import * as LocalAuthentication from "expo-local-authentication";
-import * as ImagePicker from "expo-image-picker";
 import {
   View,
   Text,
@@ -17,18 +16,17 @@ import {
   ScrollView,
   Modal,
   Alert,
-  Image,
 } from "react-native";
 
 const PersonalInfoForm = () => {
   const [name, setName] = useState("");
+  const [apellidos, setApellidos] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
   const [message, setMessage] = useState("");
   const [keyboardOpen, setKeyboardOpen] = useState(false);
   const [termsAccepted, setTermsAccepted] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [photo, setPhoto] = useState(null);
 
   useEffect(() => {
     const keyboardDidShowListener = Keyboard.addListener(
@@ -39,11 +37,21 @@ const PersonalInfoForm = () => {
       "keyboardDidHide",
       () => setKeyboardOpen(false)
     );
-
+    const testNetwork = async () => {
+      try {
+        const response = await fetch("https://backend-integradora.vercel.app/api/contactus");
+        const data = await response.json();
+        console.log(data);  // Ver los datos en la consola
+      } catch (error) {
+        console.error("Error de red:", error);
+      }
+    };
+    testNetwork();
     return () => {
       keyboardDidShowListener.remove();
       keyboardDidHideListener.remove();
     };
+
   }, []);
 
   const authenticate = async () => {
@@ -70,10 +78,41 @@ const PersonalInfoForm = () => {
 
     const isAuthenticated = await authenticate();
     if (isAuthenticated) {
-      console.log("Formulario enviado");
-      // Aquí puedes añadir el código para enviar el formulario
+      await submitForm();
     } else {
       Alert.alert("Error de autenticación", "No se pudo verificar la huella digital.");
+    }
+  };
+
+  const submitForm = async () => {
+    const formData = {
+      nombre: name,
+      apellidos: apellidos,
+      correo: email,
+      telefono: phone,
+      mensaje: message,
+    };
+
+
+    try {
+      const response = await fetch("https://backend-integradora.vercel.app/api/contactus", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData)
+      });
+
+      const result = await response.json();
+      if (response.ok) {
+        Alert.alert("Éxito", "Formulario enviado con éxito");
+      } else {
+        console.log(result);
+        Alert.alert("Error", result.message || "Error al enviar el formulario");
+      }
+    } catch (error) {
+      console.error("Error de red al enviar el formulario:", error);
+      Alert.alert("Error", "No se pudo enviar el formulario");
     }
   };
 
@@ -91,27 +130,6 @@ const PersonalInfoForm = () => {
   };
 
   const openTermsModal = () => setIsModalVisible(true);
-
-  const takePhoto = async () => {
-    const { status } = await ImagePicker.requestCameraPermissionsAsync();
-    if (status !== "granted") {
-      Alert.alert("Permiso denegado", "Se requiere acceso a la cámara para tomar una foto.");
-      return;
-    }
-
-    const result = await ImagePicker.launchCameraAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      // Obtén la URI de la imagen capturada
-      setPhoto(result.assets[0].uri); // Usamos result.assets[0].uri para acceder a la imagen
-    } else {
-      console.log("Captura de imagen cancelada");
-    }
-  };
 
   return (
     <KeyboardAvoidingView
@@ -135,12 +153,17 @@ const PersonalInfoForm = () => {
       >
         <ScrollView contentContainerStyle={styles.container}>
           <Text style={styles.title}>Contáctanos</Text>
-
           <TextInput
             style={styles.input}
             placeholder="Nombre"
             value={name}
             onChangeText={setName}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Apellidos"
+            value={apellidos}
+            onChangeText={setApellidos}
           />
           <TextInput
             style={styles.input}
@@ -163,12 +186,6 @@ const PersonalInfoForm = () => {
             onChangeText={setMessage}
             multiline
           />
-
-          <TouchableOpacity style={styles.photoButton} onPress={takePhoto}>
-            <Text style={styles.photoButtonText}>Tomar foto</Text>
-          </TouchableOpacity>
-
-          {photo && <Image source={{ uri: photo }} style={styles.photo} />}
 
           <View style={styles.termsContainer}>
             <TouchableOpacity onPress={openTermsModal}>
@@ -248,30 +265,9 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 4,
     flex: 1,
-    marginLeft: 10,
   },
-  buttonText: { color: "#ff8e4c", fontWeight: "bold" },
-  buttonText2: { color: "#fff", fontWeight: "bold" },
-  photoButton: {
-    backgroundColor: "#ff8e4c",
-    padding: 10,
-    borderRadius: 4,
-    alignItems: "center",
-    marginVertical: 10,
-  },
-  photoButtonText: { color: "#fff", fontWeight: "bold" },
-  photo: {
-    width: "100%",
-    height: 200,
-    borderRadius: 10,
-    marginTop: 10,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "rgba(0,0,0,0.5)",
-  },
-  modalContent: { backgroundColor: "white", padding: 20, borderRadius: 10 },
+  buttonText: { color: "#333", textAlign: "center" },
+  buttonText2: { color: "white", textAlign: "center" },
 });
 
 export default PersonalInfoForm;
